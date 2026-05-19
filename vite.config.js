67aeaadf1001048d0eae8e4d89ef9ed541e2bf78
terrysf-web <terrysf@gmail.com>
2026-05-19 13:22:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // IMPORTANT:
-// If your GitHub repository name is not "dental-recovery",
+// If your GitHub repository name is not "recovery",
 // change the base value below to "/YOUR-REPO-NAME/".
 export default defineConfig({
   plugins: [react()],

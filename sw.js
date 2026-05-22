ad7440a32d44223e0f459118cfe58472fbde0eb3
terrysf-web <terrysf@gmail.com>
2026-05-21 18:08:21 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE='recovery-v55';
+const CACHE='recovery-v56';
 self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
